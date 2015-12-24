44327be99ee394eb24283c6331b61ce081ae31df
Cristian Consonni <kikkocristian@gmail.com>
2015-12-24 18:52:10 +0100

    Add window.location.href in gmtpun.js

--- public_html/static/js/gmtpun.js
+++ public_html/static/js/gmtpun.js
@@ -4,15 +4,17 @@ $(function() {
 
         $('#submit').attr('disabled', 'disabled');
 
+
         $.ajax({
                 type: "POST",
-                url: "/gmtpun/",
+                url: window.location.href + "/",
                 data: $(this).serialize(),
                 success: function(post_response) {
                     $('#submit').removeAttr('disabled');
                     $('#results-placeholder').hide().html(post_response).fadeIn(1500);
                     },
                 });
+
 
         return false;
         });
