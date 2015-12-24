a3e839c16ccf20a5c3341f75cb0ec53911a1d1f1
Cristian Consonni <kikkocristian@gmail.com>
2015-12-24 01:21:04 +0100

    Simply javascript

--- public_html/js/gmtpun.js
+++ public_html/js/gmtpun.js
@@ -1,17 +1,17 @@
-jQuery(document).ready(function() {
-jQuery(".inputform").submit(function( event ) {
+$(function() {
+$(".inputform").submit(function( event ) {
 
         event.preventDefault();
 
-        jQuery('#submit').attr('disabled', 'disabled');
+        $('#submit').attr('disabled', 'disabled');
 
-        jQuery.ajax({
+        $ajax({
                 type: "POST",
-                url: "/gmtpun/",
+                url: "/",
                 data: $(this).serialize(),
                 success: function(post_response) {
-                    jQuery('#submit').removeAttr('disabled');
-                    jQuery('#results-placeholder').hide().html(post_response).fadeIn(1500);
+                    $('#submit').removeAttr('disabled');
+                    $('#results-placeholder').hide().html(post_response).fadeIn(1500);
                     },
                 });
 
